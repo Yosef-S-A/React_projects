c97171105bd917da53d309f2510af4fe19444742
Yosef-S-A <yosefsamuel22@gmail.com>
2023-09-25 06:17:56 +0300

    initial setup

---
+++ javascript-refresher/assets/scripts/app.js
@@ -0,0 +1,7 @@
+// import apikey, { name } from "./util.js";
+import * as util from "./util.js";
+
+// console.log("apiKey: ", apikey);
+// console.log("name: ", name);
+console.log("apikey: ", util.default);
+console.log("name: ", util.name);
---
+++ javascript-refresher/assets/scripts/util.js
@@ -0,0 +1,3 @@
+// export let apikey = "fjslslajdsklj";
+export default "fjslslajdsklj"; // exported once and as a default value
+export let name = "Yosef";
