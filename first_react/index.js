524ac535a17389926a01c387322ac41f33e55e4c
Yosef-S-A <yosefsamuel22@gmail.com>
2023-12-05 18:04:32 +0300

    imperative method

--- first_react/index.js
+++ first_react/index.js
@@ -22,4 +22,19 @@ ReactDOM.render(
         <MainContent />
     </div>,
     document.getElementById("components")
-)
+);
+
+// Challenge - recreate ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
+// in vanilla JS by creating and appending an h1 to our div#root
+
+/*
+- Create a new h1 element
+- Give it some textContent
+- Give it a class name of "header"
+- append it as a child of the div#root
+*/
+
+const h1 = document.createElement("h1");
+h1.textContent = "This is an imperative way to program";
+h1.className = "header"
+document.getElementById("imperative").append(h1);
